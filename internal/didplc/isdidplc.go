e59b3765b34e70c1f4d6c4af23fae79d566f71d0
Charles Iliya Krempeaux <iamreiver@gmail.com>
2023-05-01 08:50:04 -0700

    initial commits

--- internal/didplc/isdidplc.go
+++ internal/didplc/isdidplc.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 )
 
+// IsDIDPlaceholder returns whether the passed value is a DID Placeholder.
 func IsDIDPlaceholder(did string) bool {
 	return strings.HasPrefix(did, "did:plc:")
 }
