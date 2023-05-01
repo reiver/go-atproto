52098a307bbc38eb32e8732402bec6cb59d60986
Charles Iliya Krempeaux <iamreiver@gmail.com>
2023-05-01 16:18:35 -0700

    initial commits

--- xrpc/srv/methodurl.go
+++ xrpc/srv/methodurl.go
@@ -1,11 +1,27 @@
 package xrpcsrv
 
-func MethodURL(baseurl string, methodID string) string {
-	if "" == baseurl {
+// MethodURL return the BlueSky AT-Protocol method-URL based on the service-endpoint and method-ID.
+//
+// Most likely you usually won't use this function directly, but would use it indirectly, by calling something that calls it.
+//
+// For example, if the service-endpoint is:
+//
+//	"https://bsky.social"
+//
+// And the method-ID is:
+//
+//	"com.atproto.server.createSession"
+//
+// Then MethodURL would return:
+//
+//	"https://bsky.social/xrpc/com.atproto.server.createSession"
+//
+func MethodURL(serviceEndpoint string, methodID string) string {
+	if "" == serviceEndpoint {
 		return ""
 	}
 
-	var url string = baseurl
+	var url string = serviceEndpoint
 
 	if '/' != url[len(url)-1] {
 		url += "/"
