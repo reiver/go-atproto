97cf3c89977b1c81ea724e5ec55d9222db99c340
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-09-03 14:54:07 -0700

    cosmetic change

--- com/atproto/sync/errors.go
+++ com/atproto/sync/errors.go
@@ -6,10 +6,10 @@ import (
 
 const (
 	errNilDecoder                   = erorr.Error("atproto: nil decoder")
-	errNilSubscriptionMessageHeader = erorr.Error("atproto: nil subscription-message-header")
 	errNilPayload                   = erorr.Error("atproto: nil payload")
 	errNilReader                    = erorr.Error("atproto: nil reader")
 	errNilReceiver                  = erorr.Error("atproto: nil receiver")
+	errNilSubscriptionMessageHeader = erorr.Error("atproto: nil subscription-message-header")
 	errNoBlocks                     = erorr.Error("atproto: no blocks")
 	errBlocksNotBytes               = erorr.Error("atproto: blocks not bytes")
 )
