b2167ad7a8a2e5d895398d78e2692a6eeab0a4c6
Charles Iliya Krempeaux <iamreiver@gmail.com>
2024-09-04 13:53:24 -0700

    car.Iterator

--- internal/car/errors.go
+++ internal/car/errors.go
@@ -7,6 +7,8 @@ import (
 const (
 	errNilBlock        = erorr.Error("atproto: nil block")
 	errNilBlockRawData = erorr.Error("atproto: nil block raw-data")
+	errNilBytes        = erorr.Error("atproto: nil bytes")
 	errNilCarReader    = erorr.Error("atproto: nil car-reader")
+	errNilReader       = erorr.Error("atproto: nil reader")
 	errNilReceiver     = erorr.Error("atproto: nil receiver")
 )
--- internal/car/iterator.go
+++ internal/car/iterator.go
@@ -1,6 +1,9 @@
 package car
 
 import (
+	"bytes"
+	"io"
+
 	ipld_car "github.com/ipld/go-car"
 	"github.com/reiver/go-erorr"
 
@@ -18,6 +21,40 @@ type Iterator struct {
 }
 
 var _ iter.Iterator = &Iterator{}
+
+func NewIteratorFromBytes(value []byte) (*Iterator, error) {
+	if nil == value {
+		return nil, errNilBytes
+	}
+
+	var carReader *ipld_car.CarReader
+	{
+		var reader io.Reader = bytes.NewReader(value)
+		if nil == reader {
+			return nil, errNilReader
+		}
+
+		var err error
+
+		carReader, err = ipld_car.NewCarReader(reader)
+		if nil != err {
+			return nil, erorr.Errorf("atproto: problem creating CAR (Content Addressable aRchives) reader: %w", err)
+		}
+	}
+
+	return NewIteratorFromCarReader(carReader)
+}
+
+func NewIteratorFromCarReader(carReader *ipld_car.CarReader) (*Iterator, error) {
+	if nil == carReader {
+		return nil, errNilCarReader
+	}
+
+	var iterator Iterator
+	iterator.CarReader = carReader
+
+	return &iterator, nil
+}
 
 func (receiver *Iterator) Close() error {
 	if nil == receiver {
